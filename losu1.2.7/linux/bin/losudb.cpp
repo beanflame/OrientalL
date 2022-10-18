f9c0458baae5ad2e601a1aec9465401ce561bc7f
chen-chaochen <3174025065@qq.com>
2022-10-18 10:44:03 +0800

    Compiler sources on linux

--- losu1.2.7/linux/bin/losudb.cpp
+++ losu1.2.7/linux/bin/losudb.cpp
@@ -29,21 +29,21 @@ using namespace std;
 
 
 
-//  Windows下编译时取消以下注释
+/*  Windows下编译时取消以下注释
 #include<windows.h>
 string LS_ROOT = "C:/Losu/";
 string LS_LIB_PATH = "C:/Losu/lib/";
 string LS_LSI_PATH = "C:/Losu/lsi/";
 string LS_VM_PATH  = "C:/Losu/lvm/";
-//
+*/
 
-/* linux下编译时取消以下注释
+// linux下编译时取消以下注释
 #include<dlfcn.h>
 string LS_ROOT = "/usr/local/lib/losu/";
 string LS_LIB_PATH = "/usr/local/lib/losu/lib/";
 string LS_LSI_PATH = "/usr/local/lib/losu/lsi/";
 string LS_VM_PATH  = "/usr/local/lib/losu/lvm/";
-*/
+//
 
 //至此，全部配置已完成，可以编译
 
